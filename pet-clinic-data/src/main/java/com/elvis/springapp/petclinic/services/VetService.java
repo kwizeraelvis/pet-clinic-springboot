5a952824d1a306d593ff0bd24bd8012d136fd8b0
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-16 23:08:36 +0200

    refactor commmon interface

--- pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/VetService.java
@@ -4,8 +4,5 @@ import com.elvis.springapp.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
 }
