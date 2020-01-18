4b008b84c4dee7dc270ebc7634954313fd147e3b
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-18 22:49:25 +0200

    Created Owner Index Page and Owner Controller

---
+++ pet-clinic-web/src/main/java/com/elvis/springapp/petclinic/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package com.elvis.springapp.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOfOwners(){
+        return "owners/index";
+    }
+}
