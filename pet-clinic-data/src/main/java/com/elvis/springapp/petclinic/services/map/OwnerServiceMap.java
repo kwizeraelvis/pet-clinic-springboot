07c8d6440b841bd16002113000d22834191c1605
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-18 21:07:46 +0200

    Create Service Implementations for our models

---
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.elvis.springapp.petclinic.services.map;
+
+import com.elvis.springapp.petclinic.model.Owner;
+import com.elvis.springapp.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findall();
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
