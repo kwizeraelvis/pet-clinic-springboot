ee8de83a19d1d40daaf215ba0296fb2ed92bd751
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-22 19:20:04 +0200

    Show list of vets on vet list page

--- pet-clinic-web/src/main/java/com/elvis/springapp/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/elvis/springapp/petclinic/controllers/VetController.java
@@ -1,12 +1,22 @@
 package com.elvis.springapp.petclinic.controllers;
 
+import com.elvis.springapp.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listvets(){
+    public String listvets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
