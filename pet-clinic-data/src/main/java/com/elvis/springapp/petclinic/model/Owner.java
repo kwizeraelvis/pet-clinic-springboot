536cc6a863d6202f966e2e863e34f0f4223b4e1d
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-10 18:57:34 +0200

    Created PetType Map Service

--- pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/model/Owner.java
@@ -42,3 +42,4 @@ public class Owner extends Person {
 
        
 }
+
---
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.elvis.springapp.petclinic.services;
+
+import com.elvis.springapp.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
