43621268acceb1ef0b9cf8ed43837f63d4f2b3bc
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-11 00:29:06 +0200

    Add Pets and Contact info to Owners on startup

--- pet-clinic-web/src/main/java/com/elvis/springapp/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/elvis/springapp/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.elvis.springapp.petclinic.bootstrap;
 
 import com.elvis.springapp.petclinic.model.Owner;
+import com.elvis.springapp.petclinic.model.Pet;
 import com.elvis.springapp.petclinic.model.PetType;
 import com.elvis.springapp.petclinic.model.Vet;
 import com.elvis.springapp.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.elvis.springapp.petclinic.services.PetTypeService;
 import com.elvis.springapp.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,11 +41,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Kwizera");
         owner1.setLastName("Elvis");
+        owner1.setAddress("KG344 Gasabo");
+        owner1.setCity("Kigali");
+        owner1.setTelephone("65555484");
+
+        Pet elvisPet = new Pet();
+        elvisPet.setPetType(savedDogPetType);
+        elvisPet.setOwner(owner1);
+        elvisPet.setBirthDate(LocalDate.now());
+        elvisPet.setName("Cobra");
+        owner1.getPets().add(elvisPet);
+
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Higiro");
         owner2.setLastName("Emile");
+        owner2.setAddress("KG344 Gasabo");
+        owner2.setCity("Kigali");
+        owner2.setTelephone("65555484");
+
+        Pet higirosCat = new Pet();
+        higirosCat.setPetType(savedCatPetType);
+        higirosCat.setOwner(owner2);
+        higirosCat.setBirthDate(LocalDate.now());
+        higirosCat.setName("Cobra");
+        owner2.getPets().add(higirosCat);
+
         ownerService.save(owner2);
 
         System.out.println("Owner Loaded");
