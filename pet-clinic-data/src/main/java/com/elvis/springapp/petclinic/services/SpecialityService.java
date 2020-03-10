d10dbf8594c45efe83c1c1df4c7f281e61551241
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-11 00:43:11 +0200

    Add Speciality Map Service

---
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.elvis.springapp.petclinic.services;
+
+import com.elvis.springapp.petclinic.model.Speciality;
+
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,34 @@
+package com.elvis.springapp.petclinic.services.map;
+
+import com.elvis.springapp.petclinic.model.Speciality;
+import com.elvis.springapp.petclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findall();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }}
