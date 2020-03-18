7491cbfb4bb822f3e3e652c98bd630aeece09107
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-19 01:51:50 +0200

    Implement Owner entity JPA mappings

--- pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/model/Owner.java
@@ -1,12 +1,23 @@
 package com.elvis.springapp.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
