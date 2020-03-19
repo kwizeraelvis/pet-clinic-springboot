45f8d6e4f7f008da35f49d61b5e7968c9e95e41d
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-19 23:22:53 +0200

    Implement Owner Spring Data JPA Repositories

---
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.elvis.springapp.petclinic.repositories;
+
+import com.elvis.springapp.petclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
