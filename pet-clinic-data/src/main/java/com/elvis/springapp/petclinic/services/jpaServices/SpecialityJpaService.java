9e50ee0ab2f37689d5c91718e162a73a870472d9
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-25 16:21:46 +0200

    Implement VetSpeciality spring data jpa Service

---
+++ pet-clinic-data/src/main/java/com/elvis/springapp/petclinic/services/jpaServices/SpecialityJpaService.java
@@ -0,0 +1,49 @@
+package com.elvis.springapp.petclinic.services.jpaServices;
+
+
+import com.elvis.springapp.petclinic.model.Speciality;
+import com.elvis.springapp.petclinic.repositories.SpecialityRepository;
+import com.elvis.springapp.petclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialityJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
